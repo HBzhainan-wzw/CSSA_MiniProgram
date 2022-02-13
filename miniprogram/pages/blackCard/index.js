ec46840477654c32326e51bb205c7618316ab76e
Sean Song <yufei_song@ucsb.edu>
2022-02-12 18:55:50 -0800

    v0

--- miniprogram/pages/blackCard/index.js
+++ miniprogram/pages/blackCard/index.js
@@ -5,15 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    openID: '',
+    phoneNum: -1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('1');
   },
+  permInput: function(e){
+    app.globalData.phoneNum = e.detail.value
+  },
+  clickButton: function(){
+    console.log("[GlobalData.permNum]: ",app.globalData.permNum )
+    if(app.globalData.phoneNum == undefined){
+      console.log("[permNum]: Null input")
+      return false;
+    } 
+    const db = wx.cloud.database()
+
+
+    db.collection('BlackCard').where({
+      phoneNum: parseInt(app.globalData.phoneNum)
+    }).get({
+      success: res => {
+        console.log("[searchBase success]: ", res)
+        if(res.data.length == 0){
+          console.log("[Base]false")
+          wx.showToast({
+            title: '您还不是黑卡会员，快点加入我们>_<',
+            icon: 'none',
+            duration: 6000
+          })     
+        }else{
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成

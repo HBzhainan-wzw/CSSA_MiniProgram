8624cc00573ca96d4325fe5b3993b5c5fc9b0fb6
Yifan <yifanpan@ucsb.edu>
2022-02-12 20:01:47 -0800

    Complete Phone Input

--- miniprogram/pages/blackCard/index.js
+++ miniprogram/pages/blackCard/index.js
@@ -1,4 +1,5 @@
 // pages/blackCard/index.js
+const app = getApp()
 Page({
 
   /**
@@ -8,86 +9,47 @@ Page({
     openID: '',
     phoneNum: -1
   },
-
+  phoneInput: function (e) {
+    app.globalData.phoneNum = e.detail.phoneNum
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     console.log('1');
   },
-  permInput: function(e){
+  phoneInput: function (e) {
     app.globalData.phoneNum = e.detail.value
   },
-  clickButton: function(){
-    console.log("[GlobalData.permNum]: ",app.globalData.permNum )
-    if(app.globalData.phoneNum == undefined){
-      console.log("[permNum]: Null input")
+  clickButton: function () {
+    console.log("[GlobalData.phoneNum]: ", app.globalData.phoneNum)
+    if (app.globalData.phoneNum == undefined) {
+      console.log("[phoneNum]: Null input")
       return false;
-    } 
+    }
     const db = wx.cloud.database()
-
 
     db.collection('BlackCard').where({
       phoneNum: parseInt(app.globalData.phoneNum)
     }).get({
       success: res => {
         console.log("[searchBase success]: ", res)
-        if(res.data.length == 0){
+        if (res.data.length == 0) {
           console.log("[Base]false")
           wx.showToast({
             title: '您还不是黑卡会员，快点加入我们>_<',
             icon: 'none',
             duration: 6000
-          })     
-        }else{
-
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+          })
+        } else {
+          console.log("[Base]True")
+          wx.showToast({
+            title: '您是黑卡会员，快点加入我们>_<',
+            icon: 'none',
+            duration: 6000
+          })
+        }
+      }
+    })
   }
 })
